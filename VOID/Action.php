e43ec5f4e3926990472ab3b59f8f8e9710a29936
yiny0liuyin <yi00oi9hhg@qq.com>
2019-06-12 19:25:16 +0800

    override: ver0.8 complete like plugin.

--- VOID/Action.php
+++ VOID/Action.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Action for VOID Plugin
- * 
+ *
  * @author AlanDecode | 熊猫小A
  */
 
@@ -10,35 +10,35 @@ class VOID_Action extends Typecho_Widget implements Widget_Interface_Do
     /**
      * 点赞自增
      */
-    public function up(){
-        $db = Typecho_Db::get();
-        $cid=$this->request->filter('int')->cid;
-        $views = Typecho_Cookie::get('extend_contents_views'); 
-       
-        if (!in_array($cid, $views)) {
-            if($cid){
-                try {
-                    $row = $db->fetchRow($db->select('likes')
-                        ->from('table.contents')
-                        ->where('cid = ?', $cid));
-                    $db->query($db->update('table.contents')
-                        ->rows(array('likes' => (int)$row['likes']+1))
-                        ->where('cid = ?', $cid));
-                    $this->response->throwJson("success");
-                } catch (Exception $ex) {
-                 echo $ex->getCode(); 
-             }
-         }  else {
-            echo "error";
+    public function up()
+    {
+        $this->db = Typecho_Db::get();
+        $this->prefix = $this->db->getPrefix();
+        $this->options = Typecho_Widget::widget('Widget_Options');
+        $cid = $this->request->cid;
+        if (!$cid) {
+            $this->response->throwJson(array('status' => 0, 'msg' => '请选择喜欢的文章!'));
         }
-
-        array_push($views, $cid);
-        $views = implode(',', $views);
-            Typecho_Cookie::set('extend_contents_views', $views); //记录查看cookie
+        $likes = Typecho_Cookie::get('__post_likes');
+        if (empty($likes)) {
+            $likes = array();
+        } else {
+            $likes = explode(',', $likes);
         }
+        if (!in_array($cid, $likes)) {
+            $row = $this->db->fetchRow($this->db->select('likes')->from('table.contents')->where('cid = ?', $cid)->limit(1));
+            $this->db->query($this->db->update('table.contents')->rows(array('likes' => (int)$row['likes'] + 1))->where('cid = ?', $cid));
+            array_push($likes, $cid);
+            $likes = implode(',', $likes);
+            Typecho_Cookie::set('__post_likes', $likes);
+            //记录查看cookie
+            $this->response->throwJson(array('status' => 1, 'msg' => '成功点赞!'));
+        }
+        $this->response->throwJson(array('status' => 0, 'msg' => '你已经点赞过了!'));
     }
 
-    public function action(){
+    public function action()
+    {
         $this->on($this->request->is('up'))->up();
         $this->response->goBack();
     }
--- page-note.php
+++ page-note.php
@@ -89,7 +89,6 @@ endwhile;
                 }
 
                 // 点赞实现 ajax
-
                 const Like_btn = document.querySelectorAll('.like')
                 for (let el of Like_btn) {
                     el.onclick = function (e) {
@@ -104,12 +103,15 @@ endwhile;
                             },
                             url: "<?php $this->options->siteUrl(); ?>index.php/action/void_like?up",
                             success: function (res) {
-                                if (JSON.parse(res.responseText) === "success") {
+                                if (JSON.parse(res.responseText)['status'] === 1) {
                                     that.innerHTML = parseInt(that.innerHTML) + 1
                                     ks.notice("感谢你的点赞~", {color: "green", time: 1500});
                                     that.onclick = function () {
                                         ks.notice("你的爱我已经感受到了！", {color: "yellow", time: 1500});
                                     }
+                                }
+                                else if (JSON.parse(res.responseText)['status'] === 0){
+                                    ks.notice("你的爱我已经感受到了！", {color: "yellow", time: 1500});
                                 }
                             },
                             failed: function (res) {
