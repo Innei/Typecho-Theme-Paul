7e45910dad3a30dfec52dc8b9de993878ec164d6
Innei <tukon479@gmail.com>
2019-06-18 22:00:03 +0800

    add: ver1.3 new feature. comment with ajax (beta).

--- comments.php
+++ comments.php
@@ -2,150 +2,275 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 function threadedComments($comments, $options)
 {
-    $commentClass = '';
-    if ($comments->authorId) {
-        if ($comments->authorId == $comments->ownerId) {
-            $commentClass .= ' comment-by-author';
-        } else {
-            $commentClass .= ' comment-by-user';
-        }
-    }
-    $commentLevelClass = $comments->levels > 0 ? ' comment-child' : ' comment-parent';
-    if ($comments->url) {
-        $author = '<a href="' . $comments->url . '"' . '" target="_blank"' . ' rel="external nofollow">' . $comments->author . '</a>';
+  $commentClass = '';
+  if ($comments->authorId) {
+    if ($comments->authorId == $comments->ownerId) {
+      $commentClass .= ' comment-by-author';
     } else {
-        $author = $comments->author;
+      $commentClass .= ' comment-by-user';
     }
-    ?>
-    <li id="li-<?php $comments->theId(); ?>" class="comment-body<?php
-    if ($comments->levels > 0) {
-        echo ' comment-child';
-        $comments->levelsAlt(' comment-level-odd', ' comment-level-even');
-    } else {
-        echo ' comment-parent';
-    }
-    $comments->alt(' comment-odd', ' comment-even');
-    echo $commentClass;
-    ?>">
-        <div id="<?php $comments->theId(); ?>">
-            <?php $avatar = 'https://secure.gravatar.com/avatar/' . md5(strtolower($comments->mail)) . '?s=80&r=X&d='; ?>
-            <img class="avatar" src="<?php echo $avatar ?>" alt="<?php echo $comments->author; ?>"/>
-            <div class="comment_main">
-                <?php $comments->content(); ?>
-                <div class="comment_meta">
-                    <span class="comment_author"><?php echo $author ?></span> <span
-                            class="comment_time"><?php $comments->date(); ?></span><span
-                            class="comment_reply"><?php $comments->reply(); ?></span>
-                </div>
-            </div>
+  }
+  $commentLevelClass = $comments->levels > 0 ? ' comment-child' : ' comment-parent';
+  if ($comments->url) {
+    $author = '<a href="' . $comments->url . '"' . '" target="_blank"' . ' rel="external nofollow">' . $comments->author . '</a>';
+  } else {
+    $author = $comments->author;
+  }
+  ?>
+  <li id="li-<?php $comments->theId(); ?>" class="comment-body<?php
+  if ($comments->levels > 0) {
+    echo ' comment-child';
+    $comments->levelsAlt(' comment-level-odd', ' comment-level-even');
+  } else {
+    echo ' comment-parent';
+  }
+  $comments->alt(' comment-odd', ' comment-even');
+  echo $commentClass;
+  ?>">
+    <div id="<?php $comments->theId(); ?>">
+      <?php $avatar = 'https://secure.gravatar.com/avatar/' . md5(strtolower($comments->mail)) . '?s=80&r=X&d='; ?>
+      <img class="avatar" src="<?php echo $avatar ?>" alt="<?php echo $comments->author; ?>"/>
+      <div class="comment_main">
+        <?php $comments->content(); ?>
+        <div class="comment_meta">
+          <span class="comment_author"><?php echo $author ?></span> <span
+            class="comment_time"><?php $comments->date(); ?></span><span
+            class="comment_reply"><?php $comments->reply(); ?></span>
         </div>
-        <?php if ($comments->children) { ?>
-            <div class="comment-children"><?php $comments->threadedComments($options); ?></div><?php } ?>
-    </li>
+      </div>
+    </div>
+    <?php if ($comments->children) { ?>
+      <div class="comment-children"><?php $comments->threadedComments($options); ?></div><?php } ?>
+  </li>
 <?php } ?>
 
 
 
 <?php $this->comments()->to($comments); ?>
 <article class="comment-list">
-    <h1><?php $this->commentsNum(_t('暂无评论'), _t('仅有一条评论'), _t('已有 %d 条评论')); ?></h1>
-    <!--    回复评论框-->
+  <h1><?php $this->commentsNum(_t('暂无评论'), _t('仅有一条评论'), _t('已有 %d 条评论')); ?></h1>
+  <!--    回复评论框-->
 
-    <div class="reply" id="<?php $this->respondId(); ?>" style="display: none">
-        <form method="post" action="<?php $this->commentUrl() ?>" role="form"
-              class="reply_form">
-            <?php if ($this->allow('comment')): ?>
-                <?php if ($this->user->hasLogin()): ?>
-                    <p><?php _e('登录身份: '); ?><a
-                                href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>
-                        <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?>
-                            &raquo;</a>
-                    </p>
-                <?php else: ?>
-                    <div class="col-3">
-                        <input type="text" name="author" id="author" placeholder="你叫什么~"
-                               value="<?php $this->remember('author'); ?>"
-                               required/>
-                        <input type="text" name="mail" id="mail" placeholder="邮箱~"
-                               value="<?php $this->remember('mail'); ?>"
-                               required/>
-                        <input type="text" name="url" id="url" placeholder="网站~"
-                               value="<?php $this->remember('url'); ?>"/>
-                    </div>
-                <?php endif; ?>
-                <textarea id="text" rows="8" cols="100" name="text" placeholder="回复内容 ☆´∀｀☆"
-                          required><?php $this->remember('text'); ?></textarea>
+  <div class="reply" id="<?php $this->respondId(); ?>" style="display: none">
+    <form method="post" action="<?php $this->commentUrl() ?>" role="form"
+          class="reply_form">
+      <?php if ($this->allow('comment')): ?>
+        <?php if ($this->user->hasLogin()): ?>
+          <p><?php _e('登录身份: '); ?><a
+              href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>
+            <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?>
+              &raquo;</a>
+          </p>
+        <?php else: ?>
+          <div class="col-3">
+            <input type="text" name="author" id="author" placeholder="你叫什么~"
+                   value="<?php $this->remember('author'); ?>"
+                   required/>
+            <input type="text" name="mail" id="mail" placeholder="邮箱~"
+                   value="<?php $this->remember('mail'); ?>"
+                   required/>
+            <input type="text" name="url" id="url" placeholder="网站~"
+                   value="<?php $this->remember('url'); ?>"/>
+          </div>
+        <?php endif; ?>
+        <textarea id="text" rows="8" cols="100" name="text" placeholder="回复内容 ☆´∀｀☆"
+                  required><?php $this->remember('text'); ?></textarea>
 
-                <div class="submit">
-                    <button class="btn yellow" id="submit"><i class="fa fa-paper-plane"></i> 提交</button>
-                    <span class="cancel-comment-reply">
+        <div class="submit">
+          <button class="btn yellow" id="submit"><i class="fa fa-paper-plane"></i> 提交</button>
+          <span class="cancel-comment-reply">
             <?php $comments->cancelReply(); ?>
         </span>
-                </div>
-            <?php else: ?>
-                <p>评论功能暂时关闭</p>
-            <?php endif; ?>
-
-        </form>
-    </div>
-
-    <?php if ($comments->have()): ?>
-        <?php $comments->listComments(); ?>
-        <?php $comments->pageNav('&laquo;', '&raquo;'); ?>
-    <?php endif; ?>
-</article>
-<form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" role="form">
-    <section class="post-form is-comment">
-        <h3><i class="fa fa-comments"></i>评论</h3>
-        <div class="note-comments">
-            <div id="note-m"></div>
-            <?php if ($this->allow('comment')): ?>
-
-                <?php if ($this->user->hasLogin()): ?>
-                    <p><?php _e('登录身份: '); ?><a
-                                href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>.
-                        <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a>
-                    </p>
-                <?php else: ?>
-                    <input type="text" name="author" id="author" placeholder="你叫什么~"
-                           value="<?php $this->remember('author'); ?>"
-                           required/>
-                    <input type="text" name="mail" id="mail" placeholder="邮箱~" value="<?php $this->remember('mail'); ?>"
-                           required/>
-                    <input type="text" name="url" id="url" placeholder="网站~" value="<?php $this->remember('url'); ?>"/>
-                <?php endif; ?>
-                <textarea id="text" rows="8" name="text" placeholder="谢谢评论 ☆´∀｀☆"
-                          required><?php $this->remember('text'); ?></textarea>
-                <div class="submit">
-                    <button class="btn yellow" id="submit"><i class="fa fa-paper-plane"></i> 提交</button>
-                    <button class="btn red" id="cancel-commit" type="reset"><i class="fa fa-times-circle"></i> 取消
-                    </button>
-                </div>
-
-            <?php else: ?>
-                <p>评论功能暂时关闭</p>
-            <?php endif; ?>
         </div>
-    </section>
+      <?php else: ?>
+        <p>评论功能暂时关闭</p>
+      <?php endif; ?>
+
+    </form>
+  </div>
+
+  <?php if ($comments->have()): ?>
+    <?php $comments->listComments(); ?>
+    <?php $comments->pageNav('&laquo;', '&raquo;'); ?>
+  <?php endif; ?>
+</article>
+
+<!-- 评论框 -->
+<form method="post" action="<?php $this->commentUrl() ?>" id="comment-form" role="form">
+  <section class="post-form is-comment">
+    <h3><i class="fa fa-comments"></i>评论</h3>
+    <div class="note-comments">
+      <div id="note-m"></div>
+      <?php if ($this->allow('comment')): ?>
+
+        <?php if ($this->user->hasLogin()): ?>
+          <p><?php _e('登录身份: '); ?><a
+              href="<?php $this->options->profileUrl(); ?>"><?php $this->user->screenName(); ?></a>.
+            <a href="<?php $this->options->logoutUrl(); ?>" title="Logout"><?php _e('退出'); ?> &raquo;</a>
+          </p>
+        <?php else: ?>
+          <input type="text" name="author" id="author" placeholder="你叫什么~"
+                 value="<?php $this->remember('author'); ?>"
+                 required/>
+          <input type="text" name="mail" id="mail" placeholder="邮箱~" value="<?php $this->remember('mail'); ?>"
+                 required/>
+          <input type="text" name="url" id="url" placeholder="网站~" value="<?php $this->remember('url'); ?>"/>
+        <?php endif; ?>
+        <textarea id="text" rows="8" name="text" placeholder="谢谢评论 ☆´∀｀☆"
+                  required><?php $this->remember('text'); ?></textarea>
+        <div class="submit">
+          <button class="btn yellow" id="submit"><i class="fa fa-paper-plane"></i> 提交</button>
+          <button class="btn red" id="cancel-commit" type="reset"><i class="fa fa-times-circle"></i> 取消
+          </button>
+        </div>
+
+      <?php else: ?>
+        <p>评论功能暂时关闭</p>
+      <?php endif; ?>
+    </div>
+  </section>
 </form>
 
 <script>
-    (function () {
-        const commentsReply = document.querySelectorAll('span.comment_reply > a')
-        const replyForm = document.querySelector('.reply')
-        const isComment = document.querySelector('.post-form.is-comment')
-        for (let el of commentsReply) {
-            el.addEventListener('click', () => {
-                replyForm.removeAttribute('style')
-                if (isComment.classList.contains('active')) isComment.classList.remove('active');
-                setTimeout(() => {
-                    document.getElementById('cancel-comment-reply-link').addEventListener('click', () => {
-                        replyForm.style.display = 'none';
-                    })
-                })
+  class Comment {
+
+    static comment_init() {
+      const commentsReply = document.querySelectorAll('span.comment_reply > a')
+      const replyForm = document.querySelector('.reply')
+      const isComment = document.querySelector('.post-form.is-comment')
+      for (let el of commentsReply) {
+        el.addEventListener('click', e => {
+          // 给恢复按钮绑定事件 获取parent-id
+          const href = e.target.getAttribute('href')
+          window.parentId = href.match(/replyTo=(\d+)/)[1]
+          // 弹出回复框
+          replyForm.removeAttribute('style')
+          if (isComment.classList.contains('active')) isComment.classList.remove('active');
+          setTimeout(() => {
+            document.getElementById('cancel-comment-reply-link').addEventListener('click', () => {
+              replyForm.style.display = 'none';
             })
+          })
+        })
+      }
+
+
+      const comment_reply_btns = document.querySelectorAll('.commtent_reply > a')
+      for (let item of comment_reply_btns) {
+
+      }
+    }
+  }
+
+  Comment.comment_init()
+
+  // ajax 提交评论实现方法
+
+  // 阻止默认事件
+  const form = document.getElementById('comment-form')
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    post_by_ajax(e, '#comment-form')
+  });
+
+  const reply_form = document.querySelector('.reply_form')
+  reply_form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    post_by_ajax(e, '.reply_form', true)
+  });
+
+
+  // ajax 提交
+  function post_by_ajax(e, sel, reply = false) {
+
+    const commentForm = document.querySelector(sel)
+    const post_url = e.target.getAttribute('action')
+    const cookie = document.cookie
+    const referer = window.location.href
+    const domParser = new DOMParser()
+    const dom = str => domParser.parseFromString(str, 'text/html')
+    // 如果是管理员登陆
+    if (!document.querySelector('#comment-form #author')) {
+      const text = commentForm.querySelector('#text').value
+      const data = {
+        text
+      }
+
+      ks.ajax({
+        url: post_url,
+        method: 'POST',
+        data,
+        success(res) {
+          const responseDOM = dom(res.responseText)
+          try {
+            const needPartten = responseDOM.querySelector('.comment-list').innerHTML
+            needPartten === document.querySelector('.comment-list').innerHTML ? ks.notice("请等待审核哦 φ(>ω<*) ", {
+              color: "green",
+              time: 1000
+            }) : (document.querySelector('.comment-list').innerHTML = needPartten)
+          } catch (e) {
+            ks.notice(responseDOM.querySelector('.container').innerText, {
+              color: "red",
+              time: 1500
+            })
+          }
+          Comment.comment_init()
+        },
+        failed(res) {
+          console.log(res)
+          ks.notice("(；´д｀)ゞ 失败了", {
+            color: 'red',
+            time: 1500
+          })
         }
-    })();
+      })
+    } else {
+      const author = commentForm.querySelector('#author').value
+      const mail = commentForm.querySelector('#mail').value
+      const url = commentForm.querySelector('#url').value
+      const text = commentForm.querySelector('#text').value
+
+      if (reply) {
+        data = {
+          author, mail, url, text, parent: window.parentId
+        }
+      } else {
+        data = {
+          author, mail, url, text,
+        }
+      }
+      console.log(data);
+      ks.ajax({
+        method: "POST",
+        url: post_url,
+        data,
+        success(res) {
+          const responseDOM = dom(res.responseText)
+          try {
+            const needPartten = responseDOM.querySelector('.comment-list').innerHTML
+            document.querySelector('.comment-list').innerHTML = needPartten
+            Comment.comment_init()
+          } catch (e) {
+            ks.notice(responseDOM.querySelector('.container').innerText, {
+              color: "red",
+              time: 1500
+            })
+          }
+
+        }
+        ,
+        failed(res) {
+          console.log(res)
+          ks.notice("(；´д｀)ゞ 失败了", {
+            color: 'red',
+            time: 1500
+          })
+        }
+      })
+
+    }
+    return false
+  }
 
 
 </script>
